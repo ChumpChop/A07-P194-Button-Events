39490ab20a61b92e29cb0ed39e4a4dccba9f81b1
Clive B Harrison <clive_b_harrison@outlook.com>
2021-10-22 22:42:05 +0100

    Add project files.

---
+++ WPF Button Events/MainWindow.xaml.vb
@@ -0,0 +1,26 @@
+﻿Class MainWindow
+
+    Private Sub btnSayHello_Click(ByVal sender As System.Object, _
+    ByVal e As System.Windows.RoutedEventArgs) Handles btnSayHello.Click
+
+        MessageBox.Show("Hello World!", Me.Title)
+    End Sub
+
+    Private Sub btnSayHello_MouseEnter(ByVal sender As Object, _
+        ByVal e As System.Windows.Input.MouseEventArgs) _
+        Handles btnSayHello.MouseEnter
+
+        'Change the Button text
+        btnSayHello.Content = "The mouse is here!"
+    End Sub
+
+    Private Sub btnSayHello_MouseLeave(ByVal sender As Object, _
+        ByVal e As System.Windows.Input.MouseEventArgs) _
+        Handles btnSayHello.MouseLeave
+
+        'Change the Button text
+        btnSayHello.Content = "The mouse has gone!"
+    End Sub
+
+
+End Class
---
+++ Windows Forms Button Events/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub btnSayHello_Click(ByVal sender As System.Object, _
+        ByVal e As System.EventArgs) Handles btnSayHello.Click
+
+        MessageBox.Show("Hello World!", Me.Text)
+    End Sub
+
+    Private Sub btnSayHello_MouseEnter(ByVal sender As Object, _
+        ByVal e As System.EventArgs) Handles btnSayHello.MouseEnter
+
+        'Change the Button text
+        btnSayHello.Text = "The mouse is here!"
+    End Sub
+
+    Private Sub btnSayHello_MouseLeave(ByVal sender As Object, _
+        ByVal e As System.EventArgs) Handles btnSayHello.MouseLeave
+
+        'Change the Button text
+        btnSayHello.Text = "The mouse has gone!"
+    End Sub
+
+
+End Class
